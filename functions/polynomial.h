9a9ab9b95b8f64ae2319b589f3cf6d61e41809bc
leonard04 <leonerd04.2718@gmail.com>
2023-05-30 16:27:49 -0700

    Add polynomial function

---
+++ functions/polynomial.h
@@ -0,0 +1,74 @@
+//
+// Created by leonard04 on 5/30/23.
+//
+
+#ifndef FUNCTIONS_POLYNOMIAL_H
+#define FUNCTIONS_POLYNOMIAL_H
+
+
+#include <vector>
+#include "function.h"
+
+class Polynomial : public Function {
+    std::vector<double> coefficients;
+
+    public:
+
+    Polynomial(const std::vector<double> coefficients){
+        this->coefficients = coefficients;
+    }
+
+    double evaluate(double x) const override{
+        double y = 0;
+        double x_n = 1; //x to the power of n
+
+        for(double c : coefficients){
+            y += c * x_n;
+            x_n *= x;
+        }
+        return y;
+    }
+
+    Function& differentiate() const override{
+        std::vector<double> newCoef;
+
+        for(int i = 1; i < coefficients.size(); i++){
+            newCoef.push_back(coefficients[i] * i);
+        }
+
+        static Polynomial p(newCoef);
+        return p;
+    }
+
+    std::string to_string() const override{
+        std::stringstream result;
+        double c;
+        for(int i = 0; i < coefficients.size(); i++){
+            c = coefficients[i];
+
+            if(c == 0) continue;
+
+            result << (c > 0 ? " + " : " - ");
+
+            // Don't display 1x^n, just x^n.
+            if(c != 1 && c != -1) {
+                result << abs(c); // abs(c) is used because we already accounted for negatives above
+            }
+
+            if(i == 0) continue; // Don't display cx^0, just c
+
+            result << "x";
+
+            // Don't display x^1, just x
+            if(i != 1){
+                result << "^" << i;
+            }
+        }
+
+        return result.str();
+    }
+
+};
+
+
+#endif //FUNCTIONS_POLYNOMIAL_H
