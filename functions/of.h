27bf284172ce0b01b8f6afe618d74e52ed9185ff
leonard04 <leonerd04.2718@gmail.com>
2023-05-30 18:16:58 -0700

    Implement virtual functions

--- functions/of.h
+++ functions/of.h
@@ -8,6 +8,8 @@
 
 #include "function.h"
 
+// Takes one function's output of another
+// Like outer(inner(x))
 class Of : public Function {
     Function* outer;
     Function* inner;
@@ -16,6 +18,27 @@ public:
     Of(Function* outer, Function* inner) {
         this->outer = outer;
         this->inner = inner;
+    }
+
+    double evaluate(double x) const override {
+        return outer->evaluate(inner->evaluate(x));
+    }
+
+    std::string to_string(Function* func) const override{
+        return outer->to_string(inner);
+    }
+
+    Function& differentiate() const {
+        return outer->differentiate(); // We cannot properly differentiate until we can multiply functions
+    }
+
+    Of(const Of& other){
+        this->outer = other.outer;
+        this->inner = other.inner;
+    }
+
+    Of* clone() const override{
+        return new Of(*this);
     }
 
 };
