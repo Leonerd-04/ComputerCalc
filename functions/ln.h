b3c46c797c5ce59e20f721ab8b6ce2a9a160af95
leonard04 <leonerd04.2718@gmail.com>
2023-05-30 22:53:53 -0700

    Implement natural log

---
+++ functions/ln.h
@@ -0,0 +1,33 @@
+//
+// Created by leonard04 on 5/30/23.
+//
+
+#ifndef FUNCTIONS_LN_H
+#define FUNCTIONS_LN_H
+
+
+#include "function.h"
+#include "exponential.h"
+#include <cmath>
+
+class Ln : public Function{
+public:
+    double evaluate(double x) const override{
+        return log(x);
+    }
+    std::string to_string(Function* func) const override{
+        std::string x = func == nullptr ? "x" : func->to_string();
+        return "ln(" + x + ")";
+    }
+
+    Function& differentiate() const override {
+        return *new Power(-1);
+    }
+
+    Function* clone() const override{
+        return new Ln();
+    }
+};
+
+
+#endif //FUNCTIONS_LN_H
