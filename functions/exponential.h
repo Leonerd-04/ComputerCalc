9882cebcd4fa98db7714e9a7f19349c14d039b31
leonard04 <leonerd04.2718@gmail.com>
2023-05-30 22:53:40 -0700

    Implemented simple exponential and power functions

---
+++ functions/exponential.h
@@ -0,0 +1,58 @@
+//
+// Created by leonard04 on 5/30/23.
+//
+
+#ifndef FUNCTIONS_EXPONENTIAL_H
+#define FUNCTIONS_EXPONENTIAL_H
+
+
+#include "function.h"
+#include "constant.h"
+#include "combinations/multiply.h"
+#include <cmath>
+#include <typeinfo>
+
+class Exponential : public Function{
+    double base;
+public:
+    Exponential(double base = M_E) : base(base) {}
+
+    double evaluate(double x) const override {
+        return pow(base, x);
+    }
+
+    std::string to_string(Function *func) const override{
+        std::string x = func == nullptr ? "x" : ("(" + func->to_string() + ")");
+        return std::to_string(base) + "^" + x;
+    }
+
+    Function& differentiate() const override{
+        return log(base) * *this;
+    }
+};
+
+class Power : public Function{
+    double power;
+public:
+    Power(double power) : power(power){}
+
+    double evaluate(double x) const override{
+        return pow(x, power);
+    }
+
+    std::string to_string(Function* func) const override{
+        std::string x = func == nullptr ? "x" :  "(" + func->to_string() + ")";
+        return x + "^" + std::to_string(power);
+    }
+    Multiply& differentiate() const override{
+        return power * Power(power - 1);
+    }
+
+    Power(const Power& other) : power(other.power){}
+
+    Power* clone() const override{
+        return new Power(*this);
+    }
+};
+
+#endif //FUNCTIONS_EXPONENTIAL_H
