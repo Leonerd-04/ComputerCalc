737edd9fe5e4ad4163f48e2eee38162793e158d4
leonard04 <leonerd04.2718@gmail.com>
2023-05-30 21:45:22 -0700

    Add a constant function

---
+++ functions/constant.h
@@ -0,0 +1,38 @@
+//
+// Created by leonard04 on 5/30/23.
+//
+
+#ifndef FUNCTIONS_CONSTANT_H
+#define FUNCTIONS_CONSTANT_H
+
+#include "function.h"
+
+// Useful to not need to make special cases with other classes
+class Constant : public Function {
+    const double c;
+public:
+    Constant(double c) : c(c){}
+
+    double evaluate(double x) const override {
+        return c;
+    }
+
+    std::string to_string(Function* func) const override {
+        return std::to_string(c);
+    }
+
+
+    Constant& differentiate() const override {
+        return *new Constant(0);
+    }
+
+
+    Constant(const Constant& other) : c(other.c){}
+
+    Function* clone() const override{
+        return new Constant(*this);
+    }
+};
+
+
+#endif //FUNCTIONS_CONSTANT_H
